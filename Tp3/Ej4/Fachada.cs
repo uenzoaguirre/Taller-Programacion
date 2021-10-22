aedeeefffd25a676dd8bab30945d11e132eca57c
Lihuen Segovia Grabois <lihuensg@gmail.com>
2021-10-22 18:24:38 -0300

    Fachada terminada

--- Tp3/Ej4/Fachada.cs
+++ Tp3/Ej4/Fachada.cs
@@ -9,9 +9,39 @@ namespace Ej4
     {
         FabricaEncriptadores Encriptadores=FabricaEncriptadores.Instance;
 
-        public void EncriptarCesar(string pCadena)
+        public string EncriptarCesar(string pCadena)
         {
-            
+            return Encriptadores.GetEncriptador("Cesar").Encriptar(pCadena);
         }
+        public string DesencriptarCesar(string pCadena)
+        {
+            return Encriptadores.GetEncriptador("Cesar").Desencriptar(pCadena);
+        }
+        public string EncriptarAES(string pCadena)
+        {
+            return Encriptadores.GetEncriptador("AES").Encriptar(pCadena);
+        }
+         public string DesencriptarAES(string pCadena)
+        {
+            return Encriptadores.GetEncriptador("AES").Desencriptar(pCadena);
+        }
+         public string EncriptarBlowFish(string pCadena)
+        {
+            return Encriptadores.GetEncriptador("BlowFish").Encriptar(pCadena);
+        }
+         public string DesencriptarBlowFish(string pCadena)
+        {
+            return Encriptadores.GetEncriptador("BlowFish").Desencriptar(pCadena);
+        }
+         public string EncriptarDES(string pCadena)
+        {
+            return Encriptadores.GetEncriptador("DES").Encriptar(pCadena);
+        }
+         public string DesencriptarDES(string pCadena)
+        {
+            return Encriptadores.GetEncriptador("DES").Desencriptar(pCadena);
+        }
+         
+
     }
 }
