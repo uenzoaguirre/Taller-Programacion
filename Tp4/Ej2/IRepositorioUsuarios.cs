be019f959e57f138ed19523ea4a689c20579e3e8
Lihuen Segovia Grabois <lihuensg@gmail.com>
2021-11-03 19:36:01 -0300

    Ej 2 terminado creo

---
+++ Tp4/Ej2/IRepositorioUsuarios.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ej2
+{
+    public interface IRepositorioUsuarios
+    {
+        void Agregar(Usuario pUsuario);
+        void Actualizar(Usuario pUsuario);
+        void Eliminar(string pCodigo);
+        IList<Usuario> ObtenerTodos();
+        Usuario ObtenerPorCodigo(string pCodigo);
+        IList<Usuario> ObtenerOrdenadosPor(IComparer<Usuario> pComparador);
+        IList<Usuario> ObtenerOrdenadosPorCodigo();
+    }
+    public class CompararUsuarioPorCodigo : IComparer<Usuario>
+    {
+        public int Compare(Usuario x, Usuario y)
+        {
+            return x.Codigo.CompareTo(y.Codigo);
+        }
+    }
+    public class CompararUsuarioPorNombreCompleto : IComparer<Usuario>
+    {
+        public int Compare(Usuario x, Usuario y)
+        {
+            return x.NombreCompleto.CompareTo(y.NombreCompleto);
+        }
+    }
+     public class CompararUsuarioPorCorreo : IComparer<Usuario>
+    {
+        public int Compare(Usuario x, Usuario y)
+        {
+            return x.CorreoElectronico.CompareTo(y.CorreoElectronico);
+        }
+    }
+}
