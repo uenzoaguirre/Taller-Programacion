109f12b1ab7481056b9775aee2b67c2a1b4976e7
Lihuen Segovia Grabois <lihuensg@gmail.com>
2021-11-09 18:48:08 -0300

    IRepositorioUsuario

---
+++ TpFinal/Aplication/IRepositorioUsuario.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplication
+{
+    public class IRepositorioUsuario
+    {
+        void Agregar (usuario pUsuario)
+        {
+
+        }
+        usuario ObtenerPorDni(int dni)
+        {
+
+        }
+
+        List<Usuario> Encontrar(int predicado)
+        {
+            
+        }
+
+    }
+}
