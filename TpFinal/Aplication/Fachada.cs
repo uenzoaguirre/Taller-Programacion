086203c64791668d7be1589f928d5006681b37d5
Iván Bonti <ivanbonti300@gmail.com>
2021-11-09 18:28:40 -0300

    Fachada creada

--- TpFinal/Aplication/Fachada.cs
+++ TpFinal/Aplication/Fachada.cs
@@ -12,6 +12,51 @@ namespace Aplication
 
         } 
 
-        public 
+        public void AgregarEjemplar(string codigoInventario, string isbn)
+        {
+
+        }
+        public List<DTOPrestamo> PrestamosProximosAVencer(string dni) 
+        {
+
+        } 
+
+        public List<DTOPrestamo> ListarPrestamos(string dni) 
+        {
+
+        }
+
+        public  List<DTOPrestamo> PrestamosEntreFechas(string dni, DateTime fechaInicio ,DateTime fechaFin)
+        {
+
+        }
+
+        public bool EsUsuarioAdmin(string nombreUsuario) 
+        {
+
+        }
+
+        public List<DTOEjemplar> ListarEjemplares(dictionary<string,string> datos)
+        {
+
+        } 
+
+        public List<DTOEdicion>ListarEdiciones(dictionary<string,string> datos)
+        {
+
+        } 
+
+        public void AgregarEdicion(DTOEdicion edicion) 
+        {
+            
+        }
+
+        public bool ExisteEdicion(DTOEdicion edicion) 
+        {
+
+        }
+
+
+        
     }
 }
