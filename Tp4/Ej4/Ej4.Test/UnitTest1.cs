2c9a878b0ba5509fb43e8d9a41ea5a2e443dab73
Iván Bonti <ivanbonti300@gmail.com>
2021-11-12 17:43:32 -0300

    Nose

--- Tp4/Ej4/Ej4.Test/UnitTest1.cs
+++ Tp4/Ej4/Ej4.Test/UnitTest1.cs
@@ -3,12 +3,16 @@ using Xunit;
 
 namespace Ej4.Test
 {
-    public class UnitTest1
+    public class tester
     {
         [Fact]
-        public void Test1()
+        public void EvaluadorCompuesto_Happy_Path_Ok()
         {
-            
+            Empleo pEmpleo1 = new Empleo(4500, 10 - 11 - 2021);
+            Cliente cliente1 = new Cliente( Lihuen, Segovia, 30-12-2000 , pEmpleo1); 
+            SolicitudPrestamo pSolicitud = new SolicitudPrestamo(Cliente1,200000,30);
+            EvaluadorCompuesto eCompuesto = new EvaluadorCompuesto();
+            Assert.Equal("true", eCompuesto.EsValida(pSolicitud));
         }
     }
 }
