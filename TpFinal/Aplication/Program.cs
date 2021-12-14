4129365bf75af859acbc85c5e07ea40057fde2c4
Iván Bonti <ivanbonti300@gmail.com>
2021-12-14 19:07:03 -0300

    Implementado del service ediciones

--- TpFinal/Aplication/Program.cs
+++ TpFinal/Aplication/Program.cs
@@ -10,6 +10,11 @@ namespace Aplication
            var filtros = new Dictionary<string,string>(){{"Nombre","ROWLING"}};
            var autores = ServiceAutoresOpenLibrary.Buscar(filtros);
            Console.WriteLine("Tamaños de lista: {0}",autores.Count);
+           var filtrosEdiciones = new Dictionary<string, string>() { { "ISBN", "9780140328721" } };
+           var ediciones = ServiceEdicionesOpenLibrary.Buscar(filtrosEdiciones);
+           Console.WriteLine("Tamaños de lista: {0}",ediciones.Count);
+
+            
         }
 
     }
--- TpFinal/Aplication/SERVICE/OPENLIBRARY/ServicesEdicionOpenLibrary.cs
+++ TpFinal/Aplication/SERVICE/OPENLIBRARY/ServicesEdicionOpenLibrary.cs
@@ -2,14 +2,79 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Globalization;
 
 namespace Aplication
 {
-    public class ServicesEdicionOpenLibrary
+    public class ServiceEdicionesOpenLibrary
     {
-         List<DTOEdicion> Buscar (Dictionary<string,string> pFiltros)
-         {
-             throw new NotImplementedException();
-         }
+       public static List<DTOEdicion> Buscar(Dictionary<string, string> pFiltros)
+        {
+            // Establecimiento del protocolo ssl de transporte
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            if (pFiltros.Count == 0)
+            {
+                throw new Exception("Se nececita filtrar por ISBN");
+            }
+            string mUrl = "";
+            if (pFiltros.ContainsKey("ISBN"))
+            {
+                mUrl = String.Format("https://openlibrary.org/isbn/{0}.json", pFiltros["ISBN"]);
+            }
+            List<DTOEdicion> ediciones = new List<DTOEdicion>();
+
+
+            // Se crea el request http
+            HttpWebRequest mRequest = (HttpWebRequest)WebRequest.Create(mUrl);
+
+            try
+            {
+                // Se ejecuta la consulta
+                WebResponse mResponse = mRequest.GetResponse();
+
+                // Se obtiene los datos de respuesta
+                using (Stream responseStream = mResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+
+                    // Se parsea la respuesta y se serializa a JSON a un objeto dynamic
+                    dynamic mResponseJSON = JsonConvert.DeserializeObject(reader.ReadToEnd());
+
+                    // Se iteran cada uno de los resultados.
+
+                    DTOEdicion edicion = new DTOEdicion();
+                    edicion.Isbn = pFiltros["ISBN"];
+                    edicion.Portada = String.Format("https://covers.openlibrary.org/b/isbn/{0}-L.jpg", pFiltros["ISBN"]);
+                    edicion.numeroPaginas = mResponseJSON["number_of_pages"];
+                    edicion.Edicion = mResponseJSON["revision"];
+                    edicion.FechaPublicacion = DateTime.ParseExact((string)mResponseJSON["publish_date"] , "MMMM d, yyyy" , CultureInfo.InvariantCulture);
+                    ediciones.Add(edicion); 
+
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse mErrorResponse = ex.Response;
+                using (Stream mResponseStream = mErrorResponse.GetResponseStream())
+                {
+                    StreamReader mReader = new StreamReader(mResponseStream, Encoding.GetEncoding("utf-8"));
+                    String mErrorText = mReader.ReadToEnd();
+
+                    System.Console.WriteLine("Error: {0}", mErrorText);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("Error: {0}", ex.Message);
+            }
+            return ediciones; 
+        }
     }
+
 }
+
